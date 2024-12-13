41ecc6278272c3fa5f7a2f3b915753cf1a0a0820
ujjwalb1 <ujjwal.b@scallopx.com>
2024-12-13 13:42:33 +0530

    New ABI added.

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 
 const config = {
-    rpcUrl: "https://mainnet.infura.io/v3/0d9a5a6f2bae427485965c56ad732db9",
+    rpcUrl: "https://docs.bscscan.com/misc-tools-and-utilities/public-rpc-nodes#testnet-bsc-rpc-nodes-chainid-97",
+    USDEContractAddress: "0xBF11817AE5424742B7549f76E5a6fBc65EC8032a",
+    USDTContractAddress: "0x8F57b17A604e4532f9E1FA5254Ff22a9a43F9949"
 
 }
 export default config;
